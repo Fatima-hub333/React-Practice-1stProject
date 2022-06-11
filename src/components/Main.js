a08641b61061d5f272bc26163cc75d0c57c65286
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-11 12:25:26 +0500

    Create Main part of page according to design & give styling according to that

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,16 @@
 import React from 'react'
 
 export default function Main() {
-  return <h1>Main Component</h1>
+  return (
+    <main>
+    <h1 className="main--title">Fun Facts About React</h1>
+    <ul className="main--facts">
+        <li>Was First Released in 2013</li>
+        <li>Was originally Created by Jordan Walke</li>
+        <li>Has well over 100k stars on Github</li>
+        <li>Is Maintained By Facebook</li>
+        <li>Powers Thousands of Enterprises Apps including Mobile apps.</li>
+      </ul>
+    </main>
+  )
 }
