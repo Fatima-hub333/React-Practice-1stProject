6ae27f87f779932fb2d9c72013800eb4bc4f0958
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-11 13:05:44 +0500

    Add background image in main.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Navbar() {
     return (
         <nav>
-        <img src={require('./react-logo.jpg')} className="nav--icon" alt="Logo" width="80px" />
+        <img src={require('./images/react-icon-small.png')} className="nav--icon" alt="Logo" width="80px" />
         <h3 className='nav--logo-text'> ReactFacts </h3>
         <h4 className='nav--title'> React Course - Project 1 </h4>
       </nav>
