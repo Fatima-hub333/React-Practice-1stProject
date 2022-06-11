f65cfc81f2e1fc15f4b74ae79287008a5d827452
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-06-11 11:48:05 +0500

    Add Navbar designaccording to design & give styling to it

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Navbar() {
     return (
         <nav>
-          <img src={require('./images/Logo.jpg')}
+        <img src={require('./react-logo.jpg')} className="nav--icon" alt="Logo" width="80px" />
+        <h3 className='nav--logo-text'> ReactFacts </h3>
+        <h4 className='nav--title'> React Course - Project 1 </h4>
       </nav>
   )
 }
